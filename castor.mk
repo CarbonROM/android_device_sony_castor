5b81e4c0b3e0b1edcfb0cbf925eef8cabfc74ebc
Max Weffers <rcstar6696@gmail.com>
2017-04-17 11:23:25 +0200

    castor: Add missing mixer path file
    Change-Id: I07d4a9befb441f046fd7878398e61505204ee7ab

--- castor.mk
+++ castor.mk
@@ -28,7 +28,8 @@ ro.use_data_netmgrd=true
 # Audio
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/castor/castor-vendor.mk)
