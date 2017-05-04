3a7c16b61827bff411f81e949afc3ddaa41691c1
Max Weffers <rcstar6696@gmail.com>
2017-05-04 18:11:32 +0200

    Carbonify Xperia Z2 Tablet LTE
    Change-Id: I630b470987d8dd783b7ffecce5f7e695d27f129b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_castor.mk
+    $(LOCAL_DIR)/full_castor.mk \
+    $(LOCAL_DIR)/carbon_castor.mk
--- carbon_castor.mk
+++ carbon_castor.mk
@@ -1,5 +1,11 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)	
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common_tablet.mk)
+
+# Inherit Carbon product configuration
+(call inherit-product, vendor/carbon/config/common.mk)
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 $(call inherit-product, device/sony/castor/full_castor.mk)
 
@@ -7,5 +13,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP521
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP521/SGP521:6.0.1/23.5.A.1.291/2335002082:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP521-user 6.0.1 23.5.A.1.291 2335002082 release-keys"
 
-PRODUCT_NAME := lineage_castor
+PRODUCT_NAME := carbon_castor
 PRODUCT_DEVICE := castor
