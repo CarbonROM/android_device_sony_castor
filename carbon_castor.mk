6de0e597d7498fe3d11269e2fdb81ca2b9435344
Christian Oder <myself5@carbonrom.org>
2018-05-07 20:43:11 +0200

    castor: define BUILD_FINGERPRINT for vendor and system to prevent missmatch
    Change-Id: Ice887d0152fc9b1e5af1cc217590d26bde1461d3

--- carbon_castor.mk
+++ carbon_castor.mk
@@ -7,8 +7,9 @@ $(call inherit-product, vendor/carbon/config/gsm.mk)
 $(call inherit-product, device/sony/castor/full_castor.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP521
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP521/SGP521:6.0.1/23.5.A.1.291/2335002082:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP521-user 6.0.1 23.5.A.1.291 2335002082 release-keys"
+
+BUILD_FINGERPRINT := Sony/SGP521/SGP521:6.0.1/23.5.A.1.291/2335002082:user/release-keys
 
 PRODUCT_NAME := carbon_castor
 PRODUCT_DEVICE := castor
